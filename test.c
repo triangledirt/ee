61b33693af8b33023c53c55d85530f8de1200126
dirt <dirtlove@proton.me>
2024-01-29 20:55:47 -0600

    corrected call to ee_tick()

--- test.c
+++ test.c
@@ -7,8 +7,10 @@
 int main(int argc, char *argv[])
 {
   void *context = NULL;
+  long tick;
   ee_start(context, ee_morph_start, ee_morph_stop, ee_morph_mod, ee_morph_unmod, ee_morph_exp);
-  ee_tick(TICKS);
+  for (tick = 0; tick < TICKS; tick++)
+    ee_tick();
   ee_stop();
   ;;  /*  TODO save the exp + the eval  */
   return 0;
