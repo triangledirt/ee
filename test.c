96b0aee167e730194080979387fb89345894c160
dirt <dirtlove@proton.me>
2024-02-28 00:45:03 -0600

    removed comment

--- test.c
+++ test.c
@@ -7,13 +7,12 @@ int main(int argc, char *argv[])
 {
   void *context = NULL;
   ee_start(context, ee_morph_start, ee_morph_stop, ee_morph_mod, ee_morph_unmod, ee_morph_exp);
-  ee_exp(1);
+  ee_exp(1024);
   ee_stop();
 
   ee_morph_print();
   printf("\t");
   printf("%f\n", ee_eval());
-  ;;  /*  TODO save the exp + the eval  */
 
   return 0;
 }
